f57eda479c556e533bb7b58bb105a75397c3e3ff
nashdev255 <nashdev255@gmail.com>
2025-03-30 18:18:30 +0900

    add: novisteps 10q

---
+++ novisteps/10Q/joi2023_yo1b_a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int X;
+  cin >> X;
+  cout << 24 * X << endl;
+  return 0;
+}
---
+++ novisteps/10Q/past202306_a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A, T;
+  cin >> A >> T;
+  cout << 5 * A + T << endl;
+  return 0;
+}
